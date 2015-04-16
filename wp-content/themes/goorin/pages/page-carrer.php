e07d8d54d1c4c9a2c649c44257b9162e775e2583
deepp2887 <amrinder@anattadesign.com>
2015-04-16 19:14:08 +0530

    Add page templates of location, press, and careers

---
+++ wp-content/themes/goorin/pages/page-carrer.php
@@ -0,0 +1,48 @@
+<?php
+/*
+* Template Name: Careers
+*/
+get_header(); ?>
+<div class="tertpage">
+  <div id="store-main-img">
+    <div class="slider-wrapper theme-default">
+      <div class="nivoSlider" id="slider" style="position: relative; height: 401px; background: url(&quot;http://media.goorin.com/media/wysiwyg/banner_careers.jpg&quot;) no-repeat scroll 0% 0% transparent;"><img alt="" src="http://media.goorin.com/media/wysiwyg/banner_careers.jpg" style="display: none;"><div class="nivo-caption" style="display: none; opacity: 0.8;"><p></p></div></div>
+    </div>
+  </div>
+  <div style="margin-right: auto; margin-top: 30px; overflow: hidden;">
+    <div style="width: 480px; float: left;  font-size: 123%;">
+      <p><strong><em>Who We Are:</em></strong> Established in 1895, Goorin is an authentic hat brand with a history and integrity that has been passed down for four generations. While we have been making hats for over 100 years, the story of our hat shops has just begun.&nbsp; With a focus on future retail growth, we’re looking for bright and engaging people who will join us to expand our rich heritage and take Goorin to the next level.&nbsp; We are a company of excited, motivated, boundary-pushers who love to challenge ourselves and one-another. We seek similarly creative and forward-thinking individuals to join us in creating a dynamic, fun, and inspiring environment.</p>
+      <p><em><strong>Who We Seek:</strong></em> Ideal candidates are bold individuals who are creative, independent, outgoing, have excellent customer service skills, and an eye for detail. If you are a stand-out in your field and a natural risk-taker, check out our website to see if Goorin feels like the right fit for you.</p>
+    </div>
+    <div style="width: 350px; float: left; padding-left: 80px;  font-size: 108%; " class="joblistings">
+      <h3 style="margin-top:0">Current Openings:</h3>
+      <p>Open positions are listed below. Please click the position you would like to apply for to download the job description. To apply for one of the positions below, please send an email to <a href="mailto:careers@goorin.com?subject=Job%20Application">careers@goorin.com</a>&nbsp;with the position you are applying for in the subject line<em>.</em> Attach a cover letter, your resume, and three references with your response. Thanks!</p>
+
+
+
+      <h4>Shopkeeper</h4>
+      <a href="/careers/shopkeeper-broughton-st">Broughton St (Savannah)</a><br><br>
+      <a href="/careers/shopkeeper-lincoln-road">Lincoln Road (Miami Beach)</a><br><br>
+      <a href="/careers/shopkeeper-park-slope">Park Slope (Brooklyn)</a><br><br>
+      <a href="/careers/shopkeeper-ponce-city-market">Ponce City Market (Atlanta)</a><br><br>
+      <a href="/careers/shopkeeper-santa-monica">Santa Monica</a><br><br>
+      <a href="/careers/shopkeeper-south-congress">SoCo (Vancouver)</a><br><br>
+      <a href="/careers/shopkeeper-yaletown">Yaletown (Austin)</a><br><br>
+
+      <h4>Merchants</h4><br>
+      <a href="/careers/merchant-broughton-st">Broughton St (Savannah)</a><br><br>
+      <a href="/careers/merchant-harvard-square">Harvard Square (Cambridge)</a><br><br>
+      <a href="/careers/merchant-larchmont">Larchmont (Los Angeles)</a><br><br>
+      <a href="/careers/merchant-lincoln-road">Lincoln Road (Miami Beach)</a><br><br>
+      <a href="/careers/merchant-north-beach">North Beach (San Francisco)</a><br><br>
+      <a href="/careers/merchant-pike-place">Pike Place (Seattle)</a><br><br>
+      <a href="/careers/merchant-ponce-city-market">Ponce City Market (Atlanta)</a><br><br>
+      <a href="/careers/merchant-santa-monica">Santa Monica</a><br><br>
+      <a href="/careers/merchant-union-square">Union Square (San Francisco)</a><br><br>
+      <a href="/careers/merchant-williamsburg">Williamsburg (Brooklyn)</a><br><br>
+
+      <p>We are always looking for new hatters to join our family. If you are interested, please get in contact. <a href="/files/careers/Sales_Associate_GoorinBrothers.pdf">Download the job description</a></p>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
---
+++ wp-content/themes/goorin/pages/page-help.php
@@ -0,0 +1,104 @@
+<?php
+/*
+* Template Name: Help
+*/
+get_header(); ?>
+<div class="tertpage">
+	<div style="width: 800px;">
+		<div style="float: left; width: 400px; margin-left: 50px;"><img alt="" src="http://media.goorin.com/media/wysiwyg/service.gif"></div>
+		<div style="float: right; width: 280px; padding: 100px 0 0 0; ">
+			<ul style="font-size: 131%; text-transform: uppercase;" class="helpsections">
+				<li><a href="#orders">Orders</a></li>
+				<li><a href="#questions">Questions &amp; Feedback</a></li>
+				<li><a href="#payment">Payment Options</a></li>
+				<li><a href="#giftcards">Gift Cards</a></li>
+				<li><a href="#salestax">Sales Tax</a></li>
+				<li><a href="#canadian">Canadian Customers</a></li>
+				<li><a href="#international">International Customers</a></li>
+				<li><a href="#returns">Returns</a></li>
+				<li><a href="#shipping">Shipping</a></li>
+				<li><a href="#sizing">Sizing and Fit</a></li>
+				<li><a href="#dealers">Dealers and Resellers</a></li>
+				<li><a href="#email">Email Special Offers</a></li>
+				<li><a href="#press">Press</a></li>
+			</ul>
+		</div>
+	</div>
+	<section>
+		<p><strong><a id="orders"></a>Orders</strong><br> Goorin Bros. has offered handcrafted quality hats to the general public for over 100 years, starting in 1895 when founder Cassel Goorin operated his business from a street cart in Pittsburgh, Pennsylvania. Today, Goorin Bros. offers a distinguished and resonant collection of unique, luxe hats via <a href="/hat-shops">several boutique stores</a> across the United States, and online through <a href="http://www.goorin.com">www.goorin.com.</a></p>
+	</section>
+	<section id="giftcards">
+		<p><strong>Gift Cards</strong><br>Goorin Bros. Gift Cards can be purchased in our hat shops or here at <a href="/goorin-gift-card">goorin.com</a>. For shop locations, phone numbers and hours visit <a href="/hat-shops">our shops page</a>. Please note that Gift Cards cannot be used in our Canadian shops. Gift Cards can be redeemed in U.S. shops and in checkout on goorin.com. Just look for the 'redeem a gift card' checkbox under Payment Method. Don't hesitate to contact us with any questions.</p>
+	</section>
+	
+	<section>
+		<p><strong><a id="questions"></a>Questions and Feedback</strong><br>For any issues related to your online purchase, or if you have any questions that can’t be solved by our website, Facebook community, Twitter feed or experienced retail staff, you may contact customer service at <a href="mailto:websupport@goorin.com">websupport@goorin.com</a>, or via phone at 1-877-HAT-1895 between 6AM and 6PM PT Monday - Friday, and 9AM and 3PM PT Saturday.</p>
+	</section>
+	
+	<section>
+		<p><strong><a id="payment"></a>Payment Options</strong> <br> Goorin.com currently accepts credit cards (Visa, Mastercard, American Express). We do not accept checks or money orders.<br> <br> We take great pride in our products, so that you can take pride in wearing a Goorin.&nbsp; After placing an order, you will receive two e-mails: one to verify your order, and another to notify you that your purchase has shipped. And if that’s not enough e-mails, you can <a href="http://www.goorin.com/email-signup">subscribe to our mailing list</a> where you can receive sweet nothings and even sweeter exclusive offers.</p>
+		<p>If you have a Goorin.com coupon code, you may redeem it at checkout to receive a discount on your purchase.</p>
+		<p>Goorin Bros. Gift Cards  can be redeemed in checkout on goorin.com. Just look for the 'redeem a gift card' checkbox under Payment Method.</p>
+	</section>
+	<section>
+		<p><strong><a id="salestax"></a>Sales tax</strong><br> Ever compliant, Goorin Bros. applies sales tax based on where your order is shipping to. For example, if you are in California and your order is shipping to Illinois, sales tax will apply based on the local Illinois rate. Sales tax is applied to orders shipping to California, Colorado, District of Columbia, Illinois, Louisiana, Massachusetts, Minnesota, Nevada, New York, South Carolina, Tennessee, Texas and Washington, and British Columbia and Ontario in Canada. All gift card purchased are exempt from sales tax!</p>
+	</section>
+	<section>
+		<p><strong><a id="canadian"></a>Canadian customers</strong><br> Canadian customers are responsible for the payment of duties and taxes. Brokerage fees for orders under $200 CAD will pay $10 CAD. Higher value purchases will pay a discounted but slightly higher rate. All Online Prices are in US Dollars.</p>
+	</section>
+	<section>
+		<p><strong><a id="international"></a>International customers</strong><br>
+		Despite our cosmopolitan tendencies, Goorin Bros. does not currently ship hats directly to international customers from <a href="/">www.goorin.com</a>. In the meantime, we can recommend using <a target="_blank" href="http://www.myus.com/en/our-services/">MyUS.com</a>. Serving our customers since 1997, <a target="_blank" href="http://www.myus.com/en/our-services/">MyUS.com</a> has extensive experience in properly servicing international consumers, businesses and expatriates. Please make note of the following steps you will need to take:</p>
+		<p>
+			·         Please visit <a target="_blank" href="http://www.myus.com/en/our-services/">MyUS.com</a> and create an account. There is a fee to get yourself set up, a standard account ($10.00 fee) works great for most folks.<br>
+			·         Select the Personal Shopper option (click the icon of the woman with the shopping bags).<br>
+			·         Special Note: When you are preparing to place your order, you will be given the option to select “Do Not Repack”, which we ask you to please select. This will ensure that your package arrives packed securely, in the original packing provided by our warehouse.<br>
+			·         Special Note: If you order feathers to accompany your hat, please do know that they can range between $20-50 each to import, depending upon customs.<br>
+			·         <a target="_blank" href="http://www.myus.com/en/our-services/">MyUS.com</a> will assist with processing your order “third party” (they will purchase for you, and you will pay them in turn).<br>
+			·         <a target="_blank" href="http://www.myus.com/en/our-services/">MyUS.com</a> will provide a shipment address in the US that will act as a forwarding point to you. We will ship to that address, and <a target="_blank" href="http://www.myus.com/en/our-services/">MyUs.com</a> will provide you with tracking and return/exchange support from that point forward.
+		</p>
+		<p>Please also do email our support team at <a href="mailto:websupport@goorin.com">websupport@goorin.com</a> with any questions, we are happy to help!</p>
+	</section>
+	<section>
+		<p><strong><a id="returns"></a>Goorin Bros. Online Returns</strong></p>
+		<p class="p2">Keeping true to our belief that there’s a hat for every head, we take pride in the quality of our craftsmanship, and we want to ensure that you find the hat that best fits you. We will happily refund any merchandise purchased from Goorin Bros. Hat Shops or through <a href="/">www.goorin.com</a>. Returns with the original receipt will be credited in the original form of payment for the amount paid. Returns with a Gift Receipt will be issued a Gift Card. Returns without a receipt will be offered a store credit or exchange for the current selling price. Returns can be made to the online returns address noted below or to any Goorin Bros. hat shop - for locations and shop hours visit <a href="/hat-shops">our shops page</a>. We offer a 1 time price adjustment on merchandise purchased within 14 days.</p>
+		<p class="p1"><strong>Returns by Mail:</strong></p>
+		<p class="p2">Follow these simple steps to return your Goorin Bros. purchase:</p>
+		<p class="p2"><span> </span>1. Please download, print out and <a href="http://www.goorin.com/files/Goorin Web Return Form.pdf">complete this form.</a><br> <span> </span>2. Pack up your return and include this form with the package.<br> <span> </span>3. Ship via your preferred method to:</p>
+		<p class="p2"><span> </span><em>Goorin Bros.<br> <span> </span>Attn: Online Returns<br> <span> </span>23787 Eichler St, Unit E<br> <span> </span>Hayward, CA 94545</em></p>
+		<p class="p1"><strong>Exchanges:</strong></p>
+		<p class="p2">Please note, at this time our online store can exchange items sent back to us for a different size or color of the same style only. If you would like to exchange for an alternate style, please <a href="http://www.goorin.com/files/Goorin Web Return Form.pdf">complete this form</a> and follow the steps above, and place a new order for your preferred new style.</p>
+		<p class="p1"><strong>Returns to a Shop:</strong></p>
+		<p class="p2">We are happy to accept returns for online purchases at any of our hat shop locations. For locations and shop hours visit <a href="/hat-shops">our shops page</a>.</p>
+	</section>
+	<section>
+		<p><a id="shipping"></a><strong>Shipping</strong><br> We’ll move heaven and earth to get you the hat you want when you want it, be it from one of our fabulous <a href="/hat-shops">retail locations</a>, or online at <a href="http://www.goorin.com">www.goorin.com</a>.<br><br>
+		Most in stock items ship to locations in Canada (excluding the Yukon and Northwest Territories) and the Continental US within 1 business day of ordering but some take up to 3 business days. Orders placed after 11am PST will be shipped the following business day. Standard shipping usually takes up to 3-7 business days to arrive to any destination in the contiguous 48 states. Please note that orders with more than one style may ship from different Goorin Bros. locations.<br><br> Our most expedited shipping option (overnight) is available for customers who need their hat for that job interview, important date, or last minute Vegas wedding.&nbsp;</p>
+		<p class="p1">Please note that we do not ship on the weekends, so any order placed <span class="s1">on Friday,</span> Saturday or Sunday will be shipped by the following Tuesday.</p>
+		<p>At this time, we cannot ship to Military FPO/APO Box addresses.</p>
+		</section><section>
+		<p><strong><a id="sizing"></a>Sizing and Fit</strong><br></p>
+		<p>For our hat sizing chart and instructions on measuring your head, along with tips about the types of hats that will best suit your noggin <a href="/hat-fit-guide">please click here.</a></p>
+	</section>
+	<section>
+		<p><strong><a id="dealers"></a>Dealers and Resellers</strong></p>
+		<p>Interested in selling our hats in your stores or on your website? Well, we’re pretty selective about who carries the Goorin Bros. brand, but we’d love to hear from you. Please contact our sales team at <a href="mailto:sales@goorin.com">sales@goorin.com</a> with the following information:</p>
+		<p>
+		·         Brief company history<br>
+		·         Details of retail and/or online presence (e.g. how many stores? what’s your monthly traffic/conversion?)<br>
+		·         Current product catalog and list of top performing brands
+		</p>
+		<p>Someone should respond to you within 48 hours.</p>
+	</section>
+	<section>
+		<p><strong><a id="email"></a>E-mail offers</strong></p>
+		<p><span>Join the Goorin email list to stay up to date on the latest releases, special offers, be the&nbsp;<em>first</em>&nbsp;to hear about exclusive events, sales and discounts in your local store and online.&nbsp;</span>Click here to&nbsp;<a target="_blank" href="http://www.goorin.com/email-signup">subscribe to our mailing list</a>.</p>
+		<p><span><br>You can also join us on </span><a target="_blank" href="http://www.facebook.com/GoorinBrothers">Facebook</a><span>, </span><a target="_blank" href="http://www.twitter.com/GoorinBros">Twitter</a><span> and our </span><a href="/blog">Blog</a><span>. In addtion to the exclusive offers we have weekly whisper codes, giveaways, photos and videos. A great way to learn about the Goorin Story, Heritage, and our 1333 Minna artist collective.&nbsp;</span></p>
+		</section>
+		<section>
+		<p><strong><a id="press"></a>Press</strong></p>
+		<p>For PR inquiries, please email <a href="mailto:pr@goorin.com">pr@goorin.com.</a>
+		</p>
+	</section>
+</div>
+<?php get_footer(); ?>
