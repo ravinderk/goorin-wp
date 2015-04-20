79f5fe7848347a995803869cf30b2d13468dd16d
deepp2887 <amrinder@anattadesign.com>
2015-04-20 18:00:31 +0530

    Added header html

--- wp-content/themes/goorin/header.php
+++ wp-content/themes/goorin/header.php
@@ -18,19 +18,117 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'goorin' ); ?></a>
+<div id="page" class="main">
+	<header id="header" class="master-header">
+        <div class="container">
+            <span class="saletext">SEE OUR <a href="#">Fall Sample Sale!</a></span>
+            <a href="#" class="menu-toggle">
+                <span></span>
+            </a>
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo logo-mobile"></a>
+            <a href="#" class="cart-count">2</a>
+            <nav class="nav-primary">
+                <ul>
+                    <li class="nav-primary-item nav-1 has-subnav">
+                        <a href="#" class="navlink nav-primary-item-link">Mens<span></span></a>
+                        <div class="submenu">
+                            <div class="container">
+                                <div class="colbox">
+                                    <ul class="col-1">
+                                        <h6>Featured</h6>
+                                        <li><a href="#">New</a></li>
+                                        <li><a href="#">Top Sellers</a></li>
+                                        <li><a href="#">Holiday Gifts</a></li>
+                                        <li><a href="#">XXL</a></li>
+                                        <li><a href="#">Classic Staples</a></li>
+                                        <li><a href="#">Spring Favorites</a></li>
+                                    </ul>
+                                    <ul class="col-2">
+                                        <h6>Collections</h6>
+                                        <li><a href="#">Everyday Goorin Originals</a></li>
+                                        <li><a href="#">Heritage American Made Felt</a></li>
+                                        <li><a href="#">Premium Straws & Panamas</a></li>
+                                        <li><a href="#">Outdoor Grenadier</a></li>
+                                        <li><a href="#">Ted’s Finest Flatcaps & Tweeds</a></li>
+                                        <li><a href="#">Artist Collection</a></li>
+                                        <li><a href="#">Ltd</a></li>
+                                        <li><a href="#">Kids</a></li>
+                                    </ul>
+                                    <ul class="col-3">
+                                        <h6>Shapes</h6>
+                                        <li><a href="#">Fedora</a></li>
+                                        <li><a href="#">Flatcap</a></li>
+                                        <li><a href="#">Baseball</a></li>
+                                        <li><a href="#">Cadet</a></li>
+                                    </ul>
+                                    <figure>
+                                        <a href="#">
+                                            <img src="assets/images/fall-winter-img.png" alt="">
+                                        </a> 
+                                        <a href="#">
+                                            <p>Fall/Winter Commuter Collection</p>
+                                        </a>  
+                                    </figure>
+                                </div>
+                            </div>    
+                        </div>
+                    </li>
+                    <li class="nav-primary-item nav-2 has-subnav">
+                        <a href="#" class="navlink nav-primary-item-link">Womens <span></span></a>
+                        <div class="submenu">
+                            <div class="container">
+                                <div class="colbox">
+                                    <ul class="col-1">
+                                        <h6>Featured2</h6>
+                                        <li><a href="#">New</a></li>
+                                        <li><a href="#">Top Sellers</a></li>
+                                        <li><a href="#">Holiday Gifts</a></li>
+                                        <li><a href="#">XXL</a></li>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', 'goorin' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
+                                    </ul>
+                                    <ul class="col-2">
+                                        <h6>Collections</h6>
+                                        <li><a href="#">Everyday Goorin Originals</a></li>
+                                        <li><a href="#">Heritage American Made Felt</a></li>
+                                        <li><a href="#">Premium Straws & Panamas</a></li>
+                                        <li><a href="#">Outdoor Grenadier</a></li>
+                                    </ul>
+                                    <ul class="col-3">
+                                        <h6>Shapes</h6>
+                                        <li><a href="#">Fedora</a></li>
+                                        <li><a href="#">Flatcap</a></li>
+                                        <li><a href="#">Baseball</a></li>
+                                        <li><a href="#">Cadet</a></li>
+                                        <li><a href="#">Fedora</a></li>
+                                        <li><a href="#">Flatcap</a></li>
+                                        <li><a href="#">Baseball</a></li>
+                                        <li><a href="#">Cadet</a></li>
+                                    </ul>
+                                    <figure>
+                                        <a href="#">
+                                            <img src="assets/images/fall-winter-img.png" alt="">
+                                        </a> 
+                                        <a href="#">
+                                            <p>Fall/Winter Commuter Collection</p>
+                                        </a>    
+                                        
+                                    </figure>
+                                </div>
+                            </div>    
+                        </div>
+                    </li>
+                    <li class="nav-primary-item nav-3 logo-li"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo"></a></li>
+                    <li class="nav-primary-item nav-4"><a href="#" class="nav-primary-item-link">Shops</a></li>
+                    <li class="nav-primary-item nav-5"><a href="#" class="nav-primary-item-link">Experience</a></li>
+                </ul>
+            </nav>
+            <menu class="menu">
+                <a href="#">Sign in</a>
+                <span>•</span>
+                <a href="#">Cart</a>
+            </menu>
+        </div>
+        <div class="nav-overlay"></div>
+    </header>
+    <!--header-->
 	<div id="content" class="site-content">
