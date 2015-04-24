6187893d4691660959741e6a8034008f75910aba
deepp2887 <amrinder@anattadesign.com>
2015-04-24 14:48:44 +0530

    Added footer html

--- wp-content/themes/goorin/footer.php
+++ wp-content/themes/goorin/footer.php
@@ -9,14 +9,80 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'goorin' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'goorin' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'goorin' ), 'goorin', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer class="site-footer">
+		<div class="container">
+			<div class="footer-col-row">
+				<div class="footer-col-rigt">
+					<div class="col-1">
+						<h6>Account</h6>
+						<ul>
+							<li><a href="#">My Wishlist</a></li>
+							<li><a href="#">Order History</a></li>
+							<li><a href="#">Order Status</a></li>
+							<li><a href="#">Gift Card Balance</a></li>
+						</ul>
+					</div>
+					<!--col1-->
+					<div class="col-2">
+						<h6>Account</h6>
+						<ul>
+							<li><a href="#">Returns</a></li>
+							<li><a href="#">Shipping Information</a></li>
+							<li><a href="#">Contact Us</a></li>
+							<li><a href="#">Gift Cards</a></li>
+							<li><a href="#">Hat Size Guide</a></li>
+							<li><a href="#">Hat Care</a></li>
+						</ul>
+					</div>
+					<!--col1-->
+					<div class="col-3">
+						<h6>Account</h6>
+						<ul>
+							<li><a href="#">Locations</a></li>
+							<li><a href="#">Careers</a></li>
+							<li><a href="#">Press</a></li>
+							<li><a href="#">Blog</a></li>
+						</ul>
+					</div>
+					<!--col1-->
+				</div>
+				<div class="footer-col-left">
+					<article>
+						<h3>Keep me posted.</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</article>	
+					<div class="subscribe-form">
+						<form>
+							<div class="form-field">
+								<input type="text" palceholder="Enter your email address">
+							</div>
+							<div class="form-field">	
+								<input type="submit" class="submit-btn" value="Subscribe">
+							</div>	
+						</form>	
+					</div>
+					<p class="copyright hidden-mobile">Goorin Bros., Inc.  <a href="#">Privacy</a>  <a href="#">Terms & Conditions</a></p>
+				</div>	
+			</div>
+			<!--footer-col-row-->
+			<div class="footer-social-row">
+				<div class="footer-social-col">
+					<h6>Find Us</h6>
+					<ul>
+						<li><a href="#" class="twitter"></a></li>
+						<li><a href="#" class="facebook"></a></li>
+						<li><a href="#" class="instagram"></a></li>
+						<li><a href="#" class="pinterest"></a></li>
+						<li><a href="#" class="tumbler"></a></li>
+					</ul>
+				</div>
+			</div>
+			<!--footer-social-row-->
+			<p class="copyright visible-mobile">Goorin Bros., Inc.  <a href="#">Privacy</a>  <a href="#">Terms & Conditions</a></p>
+		</div>
+	</footer>
+	<!--site-footer-->
+	
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
