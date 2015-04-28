852d47c9e8f7abf188144f5a5c2c5449b3a29705
deepp2887 <amrinder@anattadesign.com>
2015-04-28 17:22:41 +0530

    Move home page template in folder

--- wp-content/themes/goorin/acf-layouts/homepage-hero_slider_section.php
+++ wp-content/themes/goorin/acf-layouts/homepage-hero_slider_section.php
@@ -1,11 +1,20 @@
-<?php foreach ( $field['slides'] as $hero_section_slide ) { ?>
-
-	<?php if ( $hero_section_slide['content'] ) { ?>
-		<div><?php echo $hero_section_slide['content']; ?></div>
-	<?php } ?>
-
-	<?php if ( isset( $hero_section_slide['background_image'] ) ) { ?>
-		<img src="<?php $hero_section_slide['background_image']['url']; ?>" />
-	<?php } ?>
-
-<?php } ?>
+<section class="banner">
+    <div class="banner-container">
+        <div class="banner-carousel">
+            <?php foreach ( $field['slides'] as $hero_section_slide ) { ?>
+				<div class="item">
+	                <div class="banner-item item-1">
+	                	<?php if ( $hero_section_slide['content'] ) { ?>
+							<article>
+								<?php echo $hero_section_slide['content']; ?>
+							</article>
+						<?php } ?>
+	                </div>
+	            </div>
+				<?php if ( isset( $hero_section_slide['background_image'] ) ) { ?>
+					<img src="<?php $hero_section_slide['background_image']['url']; ?>" />
+				<?php } ?>
+			<?php } ?>
+        </div>
+    </div>    
+</section>
