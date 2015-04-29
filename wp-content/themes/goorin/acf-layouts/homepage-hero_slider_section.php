9f3cb478c8d4b5b147f914d96a73cded8ba0d82b
Ashfame <mail@ashfame.com>
2015-04-29 21:20:00 +0530

    template change

--- wp-content/themes/goorin/acf-layouts/homepage-hero_slider_section.php
+++ wp-content/themes/goorin/acf-layouts/homepage-hero_slider_section.php
@@ -5,20 +5,15 @@
 				<?php if ( isset( $hero_section_slide['background_image'] ) ) { ?>
 					<div class="item">
 		                <div class="banner-item item-1" style='background-image:url("<?php echo $hero_section_slide['background_image']['url']; ?>");'>
-		                	<?php if ( $hero_section_slide['content'] ) { ?>
-								<article>
-									<h6><?php echo $hero_section_slide['pre-heading_text']; ?></h6>
-									<h2><?php echo $hero_section_slide['heading']; ?></h2>
-									<div class="slider-content">
-										<?php echo $hero_section_slide['content']; ?>
-									</div>
-									<?php if ( isset( $hero_section_slide['cta_text'] ) ) { ?>
-										<a href="<?php echo $hero_section_slide['cta_url']; ?>">
-											<?php echo $hero_section_slide['cta_text']; ?>
-										</a>
-									<?php } ?>
-								</article>
-							<?php } ?>
+							<article>
+								<h6><?php echo $hero_section_slide['pre-heading_text']; ?></h6>
+								<h2><?php echo $hero_section_slide['heading']; ?></h2>
+								<?php if ( isset( $hero_section_slide['cta_text'] ) ) { ?>
+									<a href="<?php echo $hero_section_slide['cta_url']; ?>">
+										<?php echo $hero_section_slide['cta_text']; ?>
+									</a>
+								<?php } ?>
+							</article>
 		                </div>
 		            </div>
 		        <?php } ?>    
