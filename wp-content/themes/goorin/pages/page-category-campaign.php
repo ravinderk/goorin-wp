841074486e913989cd0cd40e617a655cb2dbbc33
deepp2887 <amrinder@anattadesign.com>
2015-05-06 14:27:35 +0530

    Start working on category-campaign html

---
+++ wp-content/themes/goorin/pages/page-category-campaign.php
@@ -0,0 +1,262 @@
+<?php
+/*
+* Template Name: Category Campaign
+*/
+get_header(); ?>
+<div class="category-campaign">
+	<section class="breadcrumbs">
+		<div class="container">
+			<ul>
+				<li class="home">
+					<a title="Go to Home Page" href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a>
+					<span>/ </span>
+				</li>
+				<li>
+					<a title="" href="#">Campaigns</a>
+					<span>/ </span>
+				</li>
+				<li>
+					<strong>Rooftop Brunch</strong>
+				</li>
+			</ul>
+		</div>
+	</section>
+	<!--breadcrumbs-->
+	<section class="cat-hero-block">
+		<div class="container">
+			<figure>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/cat-hero-image.jpg">
+			</figure>
+			<article>
+				<div class="cat-sub-head">
+					the  Weekend Tradition
+				</div>
+				<h1>Rooftop Brunch</h1>
+				<div class="cat-hero-content">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					<p><a href="#">See the Collection</a></p>
+				</div>
+			</article>
+		</div>
+	</section>
+	<!--cat-hero-banner-->
+	<section class="cat-showcase-block">
+		<div class="container">
+			<div class="cat-content-main">
+				<div class="block">
+					<div class="post-box is_left block-0">
+						<figure>
+							<img src="<?php echo get_template_directory_uri(); ?>/images/cat-showcase-img1.jpg">
+						</figure>
+						<article>
+							<h4>Arrive in style</h4>
+							<div class="post-box-content">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+								<p>
+									Left: <a href="#">Clayton, $120</a>
+									Right: <a href="#">The Doctor, $92</a>
+								</p>
+							</div>
+						</article>
+					</div>
+					<div class="post-box is_right block-1">
+						<figure>
+							<img src="<?php echo get_template_directory_uri(); ?>/images/cat-showcase-img2.jpg">
+						</figure>
+						<article>
+							<h4>Make friends</h4>
+							<div class="post-box-content">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+								<p>
+									Left: <a href="#">Vinnie the Brains, $120</a>
+									Right: <a href="#">Mrs. Doubtfire, $92</a>
+								</p>
+							</div>
+						</article>
+					</div>
+					<div class="post-box is_left block-2">
+						<figure>
+							<img src="<?php echo get_template_directory_uri(); ?>/images/cat-showcase-img3.jpg">
+						</figure>
+						<article>
+							<h4>Help Set the Table</h4>
+							<div class="post-box-content">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+								<p>
+									Top: <a href="#">Vinnie the Brains, $120</a>
+									Bottom: <a href="#">Mrs. Doubtfire, $92</a>
+								</p>
+							</div>
+						</article>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--cat-showcase-->
+	<!--cat-featured-block-->
+	<section class="cat-product-block">
+		<div class="container">
+			<hgroup><em>the</em> Brunch Collection</hgroup>
+			<div class="category-products">
+				<ul class="products-grid">
+					<li class="item">
+						<a class="product-image" title="County Line Heritage Fedora" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">
+        					<img alt="County Line Heritage Fedora" src="http://store.goorin.dev/media/catalog/product/cache/1/thumbnail/200x/9df78eab33525d08d6e5fb8d27136e95/imports/products/100-3137-BLK-L01.png" id="image-6611">
+    					</a>
+    					<div class="upsellInfo">
+    						<h2 class="product-name"><a title="County Line" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">County Line</a></h2>
+						</div>
+						<div class="price-box">
+							<span class="regular-price">
+								<span class="price">$130.00</span>						
+                    		</span>	
+    					</div>
+					</li>
+					<li class="item">
+						<a class="product-image" title="County Line Heritage Fedora" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">
+        					<img alt="County Line Heritage Fedora" src="http://store.goorin.dev/media/catalog/product/cache/1/thumbnail/200x/9df78eab33525d08d6e5fb8d27136e95/imports/products/100-3137-BLK-L01.png" id="image-6611">
+    					</a>
+    					<div class="upsellInfo">
+    						<h2 class="product-name"><a title="County Line" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">County Line</a></h2>
+						</div>
+						<div class="price-box">
+							<span class="regular-price">
+								<span class="price">$130.00</span>						
+                    		</span>	
+    					</div>
+					</li>
+					<li class="item">
+						<a class="product-image" title="County Line Heritage Fedora" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">
+        					<img alt="County Line Heritage Fedora" src="http://store.goorin.dev/media/catalog/product/cache/1/thumbnail/200x/9df78eab33525d08d6e5fb8d27136e95/imports/products/100-3137-BLK-L01.png" id="image-6611">
+    					</a>
+    					<div class="upsellInfo">
+    						<h2 class="product-name"><a title="County Line" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">County Line</a></h2>
+						</div>
+						<div class="price-box">
+							<span class="regular-price">
+								<span class="price">$130.00</span>						
+                    		</span>	
+    					</div>
+					</li>
+					<li class="item">
+						<a class="product-image" title="County Line Heritage Fedora" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">
+        					<img alt="County Line Heritage Fedora" src="http://store.goorin.dev/media/catalog/product/cache/1/thumbnail/200x/9df78eab33525d08d6e5fb8d27136e95/imports/products/100-3137-BLK-L01.png" id="image-6611">
+    					</a>
+    					<div class="upsellInfo">
+    						<h2 class="product-name"><a title="County Line" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">County Line</a></h2>
+						</div>
+						<div class="price-box">
+							<span class="regular-price">
+								<span class="price">$130.00</span>						
+                    		</span>	
+    					</div>
+					</li>
+					<li class="item">
+						<a class="product-image" title="County Line Heritage Fedora" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">
+        					<img alt="County Line Heritage Fedora" src="http://store.goorin.dev/media/catalog/product/cache/1/thumbnail/200x/9df78eab33525d08d6e5fb8d27136e95/imports/products/100-3137-BLK-L01.png" id="image-6611">
+    					</a>
+    					<div class="upsellInfo">
+    						<h2 class="product-name"><a title="County Line" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">County Line</a></h2>
+						</div>
+						<div class="price-box">
+							<span class="regular-price">
+								<span class="price">$130.00</span>						
+                    		</span>	
+    					</div>
+					</li>
+					<li class="item">
+						<a class="product-image" title="County Line Heritage Fedora" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">
+        					<img alt="County Line Heritage Fedora" src="http://store.goorin.dev/media/catalog/product/cache/1/thumbnail/200x/9df78eab33525d08d6e5fb8d27136e95/imports/products/100-3137-BLK-L01.png" id="image-6611">
+    					</a>
+    					<div class="upsellInfo">
+    						<h2 class="product-name"><a title="County Line" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">County Line</a></h2>
+						</div>
+						<div class="price-box">
+							<span class="regular-price">
+								<span class="price">$130.00</span>						
+                    		</span>	
+    					</div>
+					</li>
+					<li class="item">
+						<a class="product-image" title="County Line Heritage Fedora" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">
+        					<img alt="County Line Heritage Fedora" src="http://store.goorin.dev/media/catalog/product/cache/1/thumbnail/200x/9df78eab33525d08d6e5fb8d27136e95/imports/products/100-3137-BLK-L01.png" id="image-6611">
+    					</a>
+    					<div class="upsellInfo">
+    						<h2 class="product-name"><a title="County Line" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">County Line</a></h2>
+						</div>
+						<div class="price-box">
+							<span class="regular-price">
+								<span class="price">$130.00</span>						
+                    		</span>	
+    					</div>
+					</li>
+					<li class="item">
+						<a class="product-image" title="County Line Heritage Fedora" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">
+        					<img alt="County Line Heritage Fedora" src="http://store.goorin.dev/media/catalog/product/cache/1/thumbnail/200x/9df78eab33525d08d6e5fb8d27136e95/imports/products/100-3137-BLK-L01.png" id="image-6611">
+    					</a>
+    					<div class="upsellInfo">
+    						<h2 class="product-name"><a title="County Line" href="http://store.goorin.dev/best-sellers/county-line-felt-fedora-hat">County Line</a></h2>
+						</div>
+						<div class="price-box">
+							<span class="regular-price">
+								<span class="price">$130.00</span>						
+                    		</span>	
+    					</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<!--cat-product-block-->
+	<section class="cat-featured-block">
+		<div class="container">
+			<figure>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/cat-featured-img.jpg">
+			</figure>
+			<div class="cat-featured-row">
+				<article class="cat-featured-col1">
+					<h2>Raise Your Glass</h2>
+					<div class="cat-featured-content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+					</div>
+				</article>
+				<article class="cat-featured-col2">
+					<h6>From Left to Right:</h6>
+					<ul>
+						<li><a href="#">Vinnie the Brains, $90</a></li>
+						<li><a href="#">Mrs. Doubtfire, $92</a></li>
+						<li><a href="#">Clayton, $168</a></li>
+						<li><a href="#">The Don, $130</a></li>
+						<li><a href="#">Guiillermo, $120</a></li>
+						<li><a href="#">Ron Burgandy, $94</a></li>
+					</ul>
+				</article>
+			</div>
+		</div>
+	</section>
+	<section class="cat-bottom-block">
+		<div class="container">
+			<div class="cat-bottom-col">
+				<div class="breadcrumbs">
+					<ul>
+						<li class="home">
+							<a title="Go to Home Page" href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a>
+							<span>/ </span>
+						</li>
+						<li>
+							<a title="" href="#">Campaigns</a>
+							<span>/ </span>
+						</li>
+						<li>
+							<strong>Rooftop Brunch</strong>
+						</li>
+					</ul>
+				</div>
+				<a class="backtotop" href="#">Back to Top</a>
+			</div>	
+		</div>
+	</section>
+	<!--cat-bottom-block-->
+</div>
+<?php get_footer(); ?>
