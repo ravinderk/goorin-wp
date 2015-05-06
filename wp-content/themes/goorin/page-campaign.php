7e24ac596fb99b791ec18511c282458e9b45716b
deepp2887 <amrinder@anattadesign.com>
2015-05-06 17:39:15 +0530

    Added the main page class in category page

--- wp-content/themes/goorin/page-campaign.php
+++ wp-content/themes/goorin/page-campaign.php
@@ -3,10 +3,11 @@
 Template Name: Campaign Page
 */
 
-get_header();
-
-foreach ( get_field( 'layout' ) as $field ) {
-	include locate_template( 'acf-layouts/campaign-' . $field[ 'acf_fc_layout' ] . '.php' );
-}
-
-get_footer();
+get_header();?>
+<div class="category-campaign">
+	<?php 
+	foreach ( get_field( 'layout' ) as $field ) {
+		include locate_template( 'acf-layouts/campaign-' . $field[ 'acf_fc_layout' ] . '.php' );
+	} ?>
+</div>
+<?php  get_footer(); ?>
