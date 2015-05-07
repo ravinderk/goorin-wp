375aa9e0c499ceaf56fe63c99d048f4591a7ef4b
Mridul Aggarwal <mridul_aggarwal@yahoo.co.in>
2015-05-07 17:45:25 +0530

    moving the dynamic parts around for header

--- wp-content/themes/goorin/partials/header.php
+++ wp-content/themes/goorin/partials/header.php
@@ -32,7 +32,7 @@
 			?>
 		</nav>
 		<menu class="menu">
-			<a href="http://redesign.goorin.com/customer/account/" class="menu-login-link" style="display:none"><?php echo get_theme_mod('login_text', 'LOGIN'); ?></a>
+			<a href="http://redesign.goorin.com/customer/account/" class="menu-login-link"><?php echo get_theme_mod('login_text', 'LOGIN'); ?></a>
 			<span>•</span>
 			<a href="http://redesign.goorin.com/customer/account/" class="menu-account-link"><?php echo get_theme_mod('account_text', 'Account'); ?></a>
 			<span>•</span>
@@ -43,8 +43,212 @@
 	</div>
 	<div class="nav-overlay"></div>
 </header>
+<div class="cart-dropdown">
+	<h2>Your Cart (<span class="cart-count">0</span>)</h2>
+	<div class="cart-list">
+		<div class="cart-list-row">
+			<div class="cart-list-loop">
+				<figure><img src="<?php echo get_template_directory_uri(); ?>/images/hat-men.png"></figure>
+				<article>
+					<h6>Smitty</h6>
+					<p>
+						$145<br/>
+						Color: Grey<br/>
+						Size: Large <br/>
+						Qty: 2
+					</p>
+				</article>
+				<a href="#" class="cart-delete"><span></span></a>
+			</div>
+			<!--cart-list-loop-->
+			<div class="cart-list-loop">
+				<figure><img src="<?php echo get_template_directory_uri(); ?>/images/hat-women.png"></figure>
+				<article>
+					<h6>Smitty</h6>
+					<p>
+						$145<br/>
+						Color: Grey<br/>
+						Size: Large <br/>
+						Qty: 2
+					</p>
+				</article>
+				<a href="#" class="cart-delete"><span></span></a>
+			</div>
+			<!--cart-list-loop-->
+		</div>
+		<!--cart-list-row-->
+		<div class="cart-list-row">
+			<div class="cart-list-loop">
+				<figure><img src="<?php echo get_template_directory_uri(); ?>/images/hat-men.png"></figure>
+				<article>
+					<h6>Smitty</h6>
+					<p>
+						$145<br/>
+						Color: Grey<br/>
+						Size: Large <br/>
+						Qty: 2
+					</p>
+				</article>
+				<a href="#" class="cart-delete"><span></span></a>
+			</div>
+			<!--cart-list-loop-->
+			<div class="cart-list-loop">
+				<figure><img src="<?php echo get_template_directory_uri(); ?>/images/hat-women.png"></figure>
+				<article>
+					<h6>Smitty</h6>
+					<p>
+						$145<br/>
+						Color: Grey<br/>
+						Size: Large <br/>
+						Qty: 2
+					</p>
+				</article>
+				<a href="#" class="cart-delete"><span></span></a>
+			</div>
+			<!--cart-list-loop-->
+		</div>
+		<!--cart-list-row-->
+		<div class="cart-list-row">
+			<div class="cart-list-loop">
+				<figure><img src="<?php echo get_template_directory_uri(); ?>/images/hat-men.png"></figure>
+				<article>
+					<h6>VINNIE THE BRAINS</h6>
+					<p>
+						$145<br/>
+						Color: Grey<br/>
+						Size: Large <br/>
+						Qty: 2
+					</p>
+				</article>
+				<a href="#" class="cart-delete"><span></span></a>
+			</div>
+			<!--cart-list-loop-->
+			<div class="cart-list-loop">
+				<figure><img src="<?php echo get_template_directory_uri(); ?>/images/hat-women.png"></figure>
+				<article>
+					<h6>Smitty</h6>
+					<p>
+						$145<br/>
+						Color: Grey<br/>
+						Size: Large <br/>
+						Qty: 2
+					</p>
+				</article>
+				<a href="#" class="cart-delete"><span></span></a>
+			</div>
+			<!--cart-list-loop-->
+		</div>
+		<!--cart-list-row-->
+		<div class="cart-list-row">
+			<div class="cart-list-loop">
+				<figure><img src="<?php echo get_template_directory_uri(); ?>/images/hat-men.png"></figure>
+				<article>
+					<h6>VINNIE THE BRAINS</h6>
+					<p>
+						$145<br/>
+						Color: Grey<br/>
+						Size: Large <br/>
+						Qty: 2
+					</p>
+				</article>
+				<a href="#" class="cart-delete"><span></span></a>
+			</div>
+			<!--cart-list-loop-->
+			<div class="cart-list-loop">
+				<figure><img src="<?php echo get_template_directory_uri(); ?>/images/hat-women.png"></figure>
+				<article>
+					<h6>Smitty</h6>
+					<p>
+						$145<br/>
+						Color: Grey<br/>
+						Size: Large <br/>
+						Qty: 2
+					</p>
+				</article>
+				<a href="#" class="cart-delete"><span></span></a>
+			</div>
+			<!--cart-list-loop-->
+		</div>
+		<!--cart-list-row-->
+	</div>
+	<!--cart-list-->
+	<div class="cart-total">
+		<div class="cart-message">Enjoy <span>free ground shipping</span> on all <br/>orders as our gift for you!</div>
+		<div class="cart-subtotal">Subtotal <span class="amount">$458.00</span></div>
+	</div>
+	<!--cart-total-->
+	<div class="cart-checkout">
+		<a href="#" class="btn btn-n cart-page-link">View Cart</a>
+		<a href="#" class="btn checkout-page-link">CHECKOUT</a>
+	</div>
+	<!--cart-total-->
+</div>
 <script type="text/javascript">
-	(function(document) {
-		console.log(document.cookie);
-	})(document);
+	jQuery(document).ready(function($) {
+		// Source:- https://developer.mozilla.org/en-US/docs/Web/API/document/cookie
+		var docCookies = {
+			getItem: function (sKey) {
+				if (!sKey) { return null; }
+				return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+			},
+			setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+				if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+				var sExpires = "";
+				if (vEnd) {
+					switch (vEnd.constructor) {
+						case Number:
+							sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+							break;
+						case String:
+							sExpires = "; expires=" + vEnd;
+							break;
+						case Date:
+							sExpires = "; expires=" + vEnd.toUTCString();
+							break;
+					}
+				}
+				document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+				return true;
+			},
+			removeItem: function (sKey, sPath, sDomain) {
+				if (!this.hasItem(sKey)) { return false; }
+				document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
+				return true;
+			},
+			hasItem: function (sKey) {
+				if (!sKey) { return false; }
+				return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+			},
+			keys: function () {
+				var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+				for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
+				return aKeys;
+			}
+		};
+
+		// verify the cookie
+		if(!docCookies.hasItem('hello_world'))
+			return;
+		var cookie = docCookies.getItem('hello_world');
+		cookie = JSON.parse(cookie);
+
+		// hide the corresponding link if user is logged in or not
+		if(cookie.loggedIn)
+			$('.menu-login-link').hide().next().hide();
+		else
+			$('.menu-account-link').hide().next().hide();
+
+		// number of items in the cart
+		var cartCount = 0;
+		$('.menu-cart-link .cart-count-icon, .cart-dropdown .cart-count').text(cartCount);
+
+		// cart and checkout links
+		$('.cart-dropdown .cart-checkout .cart-page-link').attr('href', cookie.cartUrl);
+		$('.cart-dropdown .cart-checkout .checkout-page-link').attr('href', cookie.checkoutUrl);
+
+		// cart total
+		$('.cart-dropdown .cart-total .cart-subtotal .amount').text(cookie.subtotal);
+
+		// loop the items in the cart
+	});
 </script>
