c03747e6095721b9282781b709cd8d2520bdc79c
deepp2887 <amrinder@anattadesign.com>
2015-05-26 15:00:32 +0530

    Added the css for category page product section

--- wp-content/themes/goorin/acf-layouts/campaign-product_section.php
+++ wp-content/themes/goorin/acf-layouts/campaign-product_section.php
@@ -1,14 +1,16 @@
 <?php
 	$heading = isset( $field['heading'] ) ? $field['heading'] : '' ;
 	$category_id = isset( $field['category_id'] ) ? $field['category_id'] : 0 ;
-	$limit = isset( $field['product_count'] ) ? $field['product_count'] : 4 ;
+	$limit = isset( $field['product_count'] ) ? $field['product_count'] : 12 ;
 	$products = get_magento_products( $category_id, $limit );
 ?>
 <?php if ( $category_id !== 0 ) { ?>
 	<section class="cat-product-block">
 		<div class="container">
 			<hgroup><?php echo $heading; ?></hgroup>
-			<div><?php echo $products ?></div>
+			<div class="category-products">
+				<?php echo $products ?>
+			</div>	
 		</div>
 	</section>
 <?php } ?>
