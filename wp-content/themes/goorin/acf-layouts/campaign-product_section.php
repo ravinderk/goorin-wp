210dfca11f00deb5b9b74fdf75344b445fe0344d
saurabh chaudhary <saurabh@anattadesign.com>
2015-06-03 20:11:34 +0530

    fixed acf field name issue

--- wp-content/themes/goorin/acf-layouts/campaign-product_section.php
+++ wp-content/themes/goorin/acf-layouts/campaign-product_section.php
@@ -1,7 +1,7 @@
 <?php
 	$heading = isset( $field['heading'] ) ? $field['heading'] : '' ;
 	$category_id = isset( $field['category_id'] ) ? $field['category_id'] : 0 ;
-	$limit = isset( $field['product_count'] ) ? $field['product_count'] : 12 ;
+	$limit = isset( $field['products_count'] ) ? $field['products_count'] : 12 ;
 	$products = get_magento_products( $category_id, $limit );
 ?>
 <?php if ( $category_id !== 0 ) { ?>
