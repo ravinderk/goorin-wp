5eb7d69560a1e79171380676e7a2054909d17897
deepp2887 <amrinder@anattadesign.com>
2015-06-16 15:13:47 +0530

    Added the sass/html of category page

--- wp-content/themes/goorin/page-categories.php
+++ wp-content/themes/goorin/page-categories.php
@@ -41,6 +41,71 @@ get_header(); ?>
             </div>
         </div>
     </section>
+    <section class="categories-blog-block">
+        <div class="container">
+            <div class="categories-blog-main">
+                <div class="categories-blog-row">
+                    <div class="categories-blog-list">
+                        <figure>
+                            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/feture-shop-img.jpg"></a>
+                        </figure>
+                        <article>
+                            <h6><a href="#">Shops</a></h6>
+                            <h4><a href="#">Williamsburg Opening</a></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
+                        </article>
+                    </div>
+                    <!--categories-blog-list-->
+                    <div class="categories-blog-list">
+                        <figure>
+                            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/feture-style-img.jpg"></a>
+                        </figure>
+                        <article>
+                            <h6><a href="#">Fashion News</a></h6>
+                            <h4><a href="#">SXSW + Coachella + You</a></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
+                        </article>
+                    </div>
+                    <!--categories-blog-list-->
+                    <div class="categories-blog-list">
+                        <figure>
+                            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/feture-shop-img.jpg"></a>
+                        </figure>
+                        <article>
+                            <h6><a href="#">from your hatshop</a></h6>
+                            <h4><a href="#">Savannah Grand Opening Bash</a></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
+                        </article>
+                    </div>
+                    <!--categories-blog-list-->
+                    <div class="categories-blog-list">
+                        <figure>
+                            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/feture-shop-img.jpg"></a>
+                        </figure>
+                        <article>
+                            <h6><a href="#">Shops</a></h6>
+                            <h4><a href="#">Williamsburg Opening</a></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
+                        </article>
+                    </div>
+                    <!--categories-blog-list-->
+                    <div class="categories-blog-list">
+                        <figure>
+                            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/feture-style-img.jpg"></a>
+                        </figure>
+                        <article>
+                            <h6><a href="#">Fashion News</a></h6>
+                            <h4><a href="#">SXSW + Coachella + You</a></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
+                        </article>
+                    </div>
+                    <!--categories-blog-list-->
+
+                </div>
+                <!--categories-blog-row-->
+            </div>
+        </div>
+    </section>
 </div>
 
 
