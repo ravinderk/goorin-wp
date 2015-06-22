e4d513465d459ceb6ea69498df938d9523fe9e48
deepp2887 <amrinder@anattadesign.com>
2015-06-22 18:51:07 +0530

    Start working on blog detail page

--- wp-content/themes/goorin/single.php
+++ wp-content/themes/goorin/single.php
@@ -6,7 +6,46 @@
  */
 
 get_header(); ?>
-
+<div class="experience-page">
+    <section class="breadcrumbs">
+        <div class="container">
+            <ul>
+                <li class="home">
+                    <a title="Go to Home Page" href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a>
+                    <span>/ </span>
+                </li>
+                <li>
+                    <a href="#">Features </a>
+                    <span>/ </span>
+                </li>
+                <li>
+                    <a href="">Styling Tips</a>
+                    <span>/ </span>
+                </li>
+                <li>
+                    <strong>The Kentucky Derby</strong>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!--breadcrumbs-->
+    <section class="experience-hero-block">
+        <div class="container">
+	        <?php $query = new WP_Query( 'tag=featured' ); ?>
+            <div class="experience-hero-detail">
+	            
+	                <div class="item">
+	                    <figure>
+		                    <a href="<?php the_permalink() ?>"><img src="<?php bloginfo('template_url'); ?>/images/detail-hero-image.jpg"></a>
+	                    </figure>
+	                    <article>
+	                        <h6><a href="<?php the_permalink() ?>">Styling Tips</a></h6>
+	                        <h1><a href="<?php the_permalink() ?>">The Kentucky Derby</a></h1>
+	                    </article>
+	                </div>
+            </div>    
+        </div>
+    </section>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -27,6 +66,5 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-<?//php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
