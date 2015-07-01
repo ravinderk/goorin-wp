94877baa5846773e92ac1433f1dd3e278d1d3858
saurabh chaudhary <saurabh@anattadesign.com>
2015-07-01 14:34:50 +0530

    made experience indiviual page dynamic

--- wp-content/themes/goorin/single.php
+++ wp-content/themes/goorin/single.php
@@ -7,6 +7,7 @@
 
 get_header(); ?>
 <div class="experience-page experience-detail-page">
+	<?php while ( have_posts() ) : the_post(); ?>
     <section class="breadcrumbs">
         <div class="container">
             <ul>
@@ -19,11 +20,12 @@ get_header(); ?>
                     <span>/ </span>
                 </li>
                 <li>
-                    <a href="">Styling Tips</a>
+	                <?php $_category = get_the_category() ?>
+                    <a href="<?php echo get_category_link( $_category[0] )?>"><?php echo $_category[0]->name ?></a>
                     <span>/ </span>
                 </li>
                 <li>
-                    <strong>The Kentucky Derby</strong>
+                    <strong><?php the_title() ?></strong>
                 </li>
             </ul>
         </div>
@@ -34,11 +36,11 @@ get_header(); ?>
 	        <div class="experience-hero-detail">
 	                <div class="item">
 	                    <figure>
-		                    <a href="<?php the_permalink() ?>"><img src="<?php bloginfo('template_url'); ?>/images/detail-hero-image.jpg"></a>
+		                    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail() ?></a>
 	                    </figure>
 	                    <article>
-	                        <h6><a href="<?php the_permalink() ?>">Styling Tips</a></h6>
-	                        <h1><a href="<?php the_permalink() ?>">The Kentucky Derby</a></h1>
+	                        <h6><?php the_category( ', ' ) ?></h6>
+	                        <h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
 	                    </article>
 	                </div>
             </div>    
@@ -48,73 +50,63 @@ get_header(); ?>
         <div class="container">
             <div class="experience-content-main">
                 <div class="exp-top-heading">
-                    <p>The “most exciting two minutes in sports” deserves an equally exciting celebration, don’t you think? Here’s how we’re gearing up for the big day.</p>
-                    <div class="exp-posted">08 / 02 / 2015 <em>by</em> kendra collins</div>
+                    <p><?php the_excerpt() ?></p>
+                    <div class="exp-posted"><?php goorin_posted_on(); ?></div>
                 </div>
                 <div class="exp-content">
-                    <?php while ( have_posts() ) : the_post(); ?>
-
                         <?php get_template_part( 'content', 'single' ); ?>
-
-                        <?php //the_post_navigation(); ?>
-
-                        <?php /*
-                            // If comments are open or we have at least one comment, load up the comment template
-                            if ( comments_open() || get_comments_number() ) :
-                                comments_template();
-                            endif;
-                        */?>
-
-                    <?php endwhile; // end of the loop. ?>
-
                 </div>
             </div>
         </div>
     </section>
     <section class="experience-detail-related">
         <div class="container">
-            <h1>You might <span>also</span> like</h1>
-            <div class="experience-preview-main">
-                <div class="experience-image-content">
-                    <figure class="experience-preview-image">
-                        <img src="http://redesign.goorin.com/wp-content/uploads/2015/04/f1.png">
-                    </figure>
-                    <article>
-                        <h4>Fashion News / Goorin Stories</h4>
-                        <div class="heading-content"><p>Fashion Week in London Part 1</p></div>
-                    
-                    </article>
-                </div>
-                <!--experience-image-content-->
-                <div class="experience-preview-content">
-                    <div class="experience-related-feature">
-                        <div class="experience-feature-content">
-                            <div class="experience-feature-box">
-                                <figure>
-                                    <a href="http://redesign.goorin.com/mens/"><img src="http://redesign.goorin.com/wp-content/uploads/2015/04/f2.png"></a>
-                                </figure>
-                                <article>
-                                    <h6><a href="http://redesign.goorin.com/mens/">Shops</a></h6>
-                                    <h4>Savannah Grand Opening Party</h4>
-                                </article>
-                            </div>
-                            <div class="experience-feature-box">
-                                <figure>
-                                    <a href="http://redesign.goorin.com/mens/"><img src="http://redesign.goorin.com/wp-content/uploads/2015/04/f2.png"></a>
-                                </figure>
-                                <article>
-                                    <h6><a href="http://redesign.goorin.com/mens/">Shops</a></h6>
-                                    <h4>Savannah Grand Opening Party</h4>
-                                </article>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--experience-preview-content-->
-            </div>
-            <!--experience-preview-main-->
+	        <?php if ( class_exists( 'AuthorRecommendedPosts' ) ) { ?>
+		        <?php
+		            $author_recommended_posts = new AuthorRecommendedPosts();
+		            $namespace = $author_recommended_posts->namespace;
+		            $recommended_ids = get_post_meta( $post->ID, $namespace, true );
+		        ?>
+	            <h1><?php $author_recommended_posts->get_option( "{$namespace}_title" ); ?></h1>
+		        <div class="experience-preview-main">
+		            <?php foreach ( $recommended_ids as $key => $id ) { ?>
+			            <?php if ( $key == 0 ) { ?>
+			                <div class="experience-image-content">
+			                    <figure class="experience-preview-image">
+			                        <a href="<?php echo get_the_permalink( $id ) ?>"><?php echo get_the_post_thumbnail( $id ) ?></a>
+			                    </figure>
+			                    <article>
+			                        <h4><?php the_category( ', ', '', $id ) ?></h4>
+			                        <div class="heading-content"><p><a href="<?php echo get_the_permalink( $id ) ?>"><?php echo get_the_title( $id ) ?></a></p></div>
+
+			                    </article>
+			                </div>
+				        <?php } else { ?>
+			                <!--experience-image-content-->
+			                <div class="experience-preview-content">
+			                    <div class="experience-related-feature">
+			                        <div class="experience-feature-content">
+			                            <div class="experience-feature-box">
+			                                <figure>
+				                                <a href="<?php echo get_the_permalink( $id ) ?>"><?php echo get_the_post_thumbnail( $id, 'post_custom_size' ) ?></a>
+			                                </figure>
+			                                <article>
+			                                    <h6><?php the_category( ', ', '', $id ) ?></h6>
+			                                    <h4><a href="<?php echo get_the_permalink( $id ) ?>"><?php echo get_the_title( $id ) ?></a></h4>
+			                                </article>
+			                            </div>
+			                        </div>
+			                    </div>
+			                </div>
+			                <!--experience-preview-content-->
+				        <?php } ?>
+			        <?php } ?>
+		        </div>
+	            <!--experience-preview-main-->
+	        <?php } ?>
         </div>
     </section>
     <!--experience-detail-related-->
+	<?php endwhile; // end of the loop. ?>
 </div>
 <?php get_footer(); ?>
