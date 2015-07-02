cce794a1e22e14c29ff51e7fedfe01e167a0b38d
deepp2887 <amrinder@anattadesign.com>
2015-07-02 21:46:59 +0530

    Added the related item images

--- wp-content/themes/goorin/archive-shops.php
+++ wp-content/themes/goorin/archive-shops.php
@@ -157,7 +157,7 @@ get_header(); ?>
 			<div class="related-preview-main">
 				<div class="related-image-content">
 					<figure class="experience-preview-image">
-						<a href="#"><img src="http://redesign.goorin.com/wp-content/uploads/2015/04/f1.png" /></a>
+						<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/f1.png" /></a>
 					</figure>
 					<article>
 						<h4>Fashion News</h4>
@@ -173,7 +173,7 @@ get_header(); ?>
 					<div class="related-feature-content">
 						<div class="related-feature-box">
 							<figure>
-								<a href="#"><img src="http://redesign.goorin.com/wp-content/uploads/2015/04/f2.png" /></a>
+								<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/f2.png" /></a>
 							</figure>
 							<article>
 								<h6>Shops</h6>
@@ -184,7 +184,7 @@ get_header(); ?>
 						</div>
 						<div class="related-feature-box">
 							<figure>
-								<a href="#"><img src="http://redesign.goorin.com/wp-content/uploads/2015/04/f3.png" /></a>
+								<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/f3.png" /></a>
 							</figure>
 							<article>
 								<h6>Shops</h6>
